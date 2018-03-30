c2fc2be2063338995ed30d027cd0d373a5257299
Mike Smiley <msmiley@alumni.rice.edu>
2018-03-29 22:26:51 -0400

    port to volante v2

--- index.js
+++ index.js
@@ -1,109 +1,102 @@
-const volante = require('volante');
 const chalk = require('chalk');
 
 //
-// Class which handles logging events from the volante.Hub and renders them to
+// handles logging events from the volante.Hub and renders them to
 // console using color provided by the chalk module.
 //
-class ConsoleLogger extends volante.Spoke {
+module.exports = {
+	name: 'ConsoleLogger',
   //
   // volante init()
   //
   init() {
-    // default options
-    this.options = {
-      timestamp: false,
-      level: 'any',
-      stringify: true,
-      srcFilter: null,
-      exitOnError: false
-    };
-
-    this.hub.on('volante.log', (obj) => {
-      this.render(obj);
-    });
-    this.hub.on('volante.debug', (obj) => {
-      this.render(obj);
-    });
-    this.hub.on('error', (obj) => {
-      this.render(obj);
-      this.options.exitOnError && process.exit(1);
-    });
-    this.hub.on('volante-console.options', (opts) => {
-      Object.assign(this.options, opts);
-    });
-
     // print header
-    console.log(chalk.blue(`Volante v${volante.version}`));
+    console.log(chalk.blue(`Volante v${this.$hub.version}`));
     console.log(chalk.blue(`console logging powered by volante-console`));
-  }
+  },
+	events: {
+    'volante.log'(obj) {
+      this.render(obj);
+    },
+    'volante.debug'(obj) {
+      this.render(obj);
+    },
+    error(obj) {
+      this.render(obj);
+      this.exitOnError && process.exit(1);
+    },
+	},
+	props: {
+    timestamp: false,
+    level: 'any',
+    stringify: true,
+    srcFilter: null,
+    exitOnError: false,
+	},
+	methods: {
+	  //
+	  // main entry point for log rendering
+	  //
+	  render(obj) {
+	    // log if any filters match
+	    if (this.checkFilters(obj)) {
+	      var line = "";
+	      if (this.timestamp) {
+	        line += chalk.magenta((new Date).toISOString());
+					line += " | ";
+	      }
+	      line += `${this.renderLevel(obj)} | ${obj.src} | `;
+	      // stringify objects
+	      if (this.stringify && typeof(obj.msg) === 'object') {
+	        line += JSON.stringify(obj.msg);
+	      } else {
+	        line += `${obj.msg}`;
+	      }
+	      console.log(line);
+	    }
+	  },
 
-  //
-  // main entry point for log rendering
-  //
-  render(obj) {
-    // log if any filters match
-    if (this.checkFilters(obj)) {
-      var line = "";
-      if (this.options.timestamp) {
-        line += chalk.magenta((new Date).toISOString() + " | ");
-      }
-      line += `${this.renderLevel(obj)} | ${obj.src} | `;
-      // stringify objects
-      if (this.options.stringify && typeof(obj.msg) === 'object') {
-        line += JSON.stringify(obj.msg);
-      } else {
-        line += `${obj.msg}`;
-      }
-      console.log(line);
-    }
-  }
+	  //
+	  // render the log level
+	  //
+	  renderLevel(obj) {
+	    switch (obj.lvl) {
+	      case 'debug':
+	        return chalk.cyan("DBG");
+	      case 'error':
+	        return chalk.red("ERR");
+	      case 'log':
+	      default:
+	        return chalk.green("LOG");
+	    }
+	  },
 
-  //
-  // render the log level
-  //
-  renderLevel(obj) {
-    switch (obj.lvl) {
-      case 'debug':
-        return chalk.cyan("DBG");
-      case 'error':
-        return chalk.red("ERR");
-      case 'log':
-      default:
-        return chalk.green("LOG");
-    }
-  }
-
-  //
-  // main entry point for log rendering
-  //
-  checkFilters(obj) {
-    // check log level filtering if not 'any'
-    if (this.options.level !== 'any') {
-      if (obj.lvl !== this.options.level) {
-        return false;
-      }
-    }
-    // check src filter
-    if (this.options.srcFilter) {
-      // see if string
-      if (typeof(this.options.srcFilter) === 'string') {
-        if (obj.src !== this.options.srcFilter) {
-          return false;
-        }
-      }
-      if (this.options.srcFilter instanceof RegExp) {
-        if (!obj.src.match(this.options.srcFilter)) {
-          return false;
-        }
-      }
-    }
-    return true;
-  }
-
+	  //
+	  // main entry point for log rendering
+	  //
+	  checkFilters(obj) {
+	    // check log level filtering if not 'any'
+	    if (this.level !== 'any') {
+	      if (obj.lvl !== this.level) {
+	        return false;
+	      }
+	    }
+	    // check src filter
+	    if (this.srcFilter) {
+	      // see if string
+	      if (typeof(this.srcFilter) === 'string') {
+	        if (obj.src !== this.srcFilter) {
+	          return false;
+	        }
+	      }
+	      if (this.srcFilter instanceof RegExp) {
+	        if (!obj.src.match(this.srcFilter)) {
+	          return false;
+	        }
+	      }
+	    }
+	    return true;
+	  },
+	}
 }
 
-//
-// exports
-//
-module.exports = ConsoleLogger;
